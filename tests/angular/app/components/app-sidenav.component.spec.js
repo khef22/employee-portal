69adb1fc8257c9d429accb615fbedeb8e9857987
keefera <keefera>
2016-12-02 22:07:09 +0800

    UI: modify header and add footer
    card #805

---
+++ tests/angular/app/components/app-sidenav.component.spec.js
@@ -0,0 +1,11 @@
+ngDescribe({
+    name: 'Test app-sidenav component',
+    modules: 'app',
+    element: '<app-sidenav></app-sidenav>',
+    tests: function (deps) {
+
+        it('basic test', () => {
+            //
+        });
+    }
+});
